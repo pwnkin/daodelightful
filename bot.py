d3987967166aae8c9fd4a6b09e6c2d2fc691ca87
Viridian Ĉielo <roux.rose@proton.me>
2023-09-18 00:29:04 -0500

    minor changes

--- bot.py
+++ bot.py
@@ -11,14 +11,10 @@ def rundiscbot():
     intents.message_content = True
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
     
-    @tree.command(name = "about", description = "Prints about info for daodelightful")
+    @tree.command(name = "about", description = "About info for daodelightful")
     async def about(interaction):
-        await interaction.response.send_message("""Hello, I'm Daodelightful! I'm a Dao De Jing quotations bot created by viridianskies! Simply say 'ddj chapter x' with x being the number of the chapter you want! You can also use the /chapter command!""")
+        await interaction.response.send_message("""Hello, I'm Daodelightful! I'm a Dao De Jing quotation bot created by viridianskies! You can use the /chapter command with the chapter number you want!""")
 
     @tree.command(name = "chapter", description = "Prints requested Daodejing chapter")
     async def chapterreq(interaction, chapter: int):
@@ -28,5 +24,5 @@ def rundiscbot():
     @client.event
     async def on_ready():
         await tree.sync()
-        print("Ready!")
+        print("Bot is up!")
     client.run(TOKEN)
