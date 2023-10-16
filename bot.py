7d544939896b1d890b54f7019d34d4dd38f51baf
pwnkin <roux.rose@proton.me>
2023-10-15 23:56:25 -0500

    formatting with new sexy linting

--- bot.py
+++ bot.py
@@ -1,33 +1,38 @@
 # Daodelightful Discord Bot
 # Prints out quotations from the Dao De Jing!
 
+
+# imports
 import discord
 import responses
 from discord import app_commands
 
+
 def rundiscbot():
-    TOKEN = 'x'
+    TOKEN = 'x' # noqa
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
-    
-    @tree.command(name = "about", description = "About info for daodelightful")
-    async def about(interaction):
-        await interaction.response.send_message("""Hello, I'm Daodelightful! I'm a Dao De Jing quotation bot created by viridianskies! You can use the /chapter command with the chapter number you want!""")
 
-    @tree.command(name = "chapter", description = "Prints requested Daodejing chapter")
+    @tree.command(name="about", description="About info for daodelightful")
+    async def about(interaction):
+        await interaction.response.send_message("""Hello, I'm Daodelightful! I'm a Dao De Jing quotation bot created by viridianskies! You can use the /chapter command with the chapter number you want!""")  # noqa
+
+    @tree.command(name="chapter", description="Prints requested chapter")
     async def chapterreq(interaction, chapter: int, translation: str):
         response = responses.hanresp(int(chapter), translation)
         await interaction.response.send_message(response)
-    
-    @tree.command(name = "translations", description = "Lists the current available translations.")
+
+    @tree.command(name="translations", description="Lists the current available translations.")  # noqa: E501
     async def translist(interaction):
-        await interaction.response.send_message("""Available translations:\n- (sm) - Stephen Mitchell\n- (gia) - Gia-Fu Feng\n- (wtc) - Wing-Tsit Chan""")
-    
+        await interaction.response.send_message("""Available translations:\n
+    - (sm) - Stephen Mitchell\n
+    - (gia) - Gia-Fu Feng\n
+    - (wtc) - Wing-Tsit Chan\n""")
+
     @client.event
     async def on_ready():
         await tree.sync()
         print("\33[32mBot is up!\033[0m")
     client.run(TOKEN)
-    
--- responses.py
+++ responses.py
@@ -1,6 +1,9 @@
 # Reponse Handler for bot.py
+
 def hasnum(s):
+    """Checks if the string contains a number"""
     return any(i.isdigit() for i in s)
+
 
 availtrans = {
     "sm": "Stephen Mitchell",
@@ -8,16 +11,28 @@ availtrans = {
     "wtc": "Wing-Tsit Chan",
 }
 
-def parse(num, trans):
+
+def parse(num: int, trans):
+    """
+    Parses the .txt files and adds each requested line
+    to make up the final message
+    sent to user.
+
+    Parameters:
+        num (int): The requested chapter from user.
+        trans (str): The requested translation from user.
+    Returns:
+        str: The result of parsing the .txt file,
+        i.e. contents of requested chapter.
+    """
     found_chap = False
     global validtrans
     validtrans = True
     global txt
-    req = "daodelightful/translations/{}ddj.txt".format(trans)
+    req = "daodelightful/translations/{}ddj.txt".format(trans) # noqa
     try:
         with open(req, 'r') as file:
             txt = file.readlines()
-            
 
     except FileNotFoundError:
         validtrans = False
@@ -32,7 +47,7 @@ def parse(num, trans):
             in_digit_sequence = False
 
             for char in line:
-                chap += char  
+                chap += char
                 if char.isdigit():
                     in_digit_sequence = True
                 else:
@@ -50,17 +65,28 @@ def parse(num, trans):
 
     return res
 
-def hanresp(chap, translation) -> str:
-    global num 
-    num = int(chap)
-    cont = parse(num, translation)
+
+def hanresp(chap: int, translation):
+    """
+    Handles the data from parse() and returns the
+    final message to be sent by the bot.
+
+    Parameters:
+        chap (str): The requested chapter from user.
+        translation (str): The requested translation from user.
+
+    Returns:
+        str: The message to be sent by the bot
+        (error message or chapter content).
+    """
+    cont = parse(chap, translation)
     atrans = availtrans[str(translation)]
     if cont:
         if validtrans:
-            pmsg = '**Chapter {0} - ({1})**\n{2}'.format(num, atrans, "\n".join(cont))
+            pmsg = '**Chapter {0} - ({1})**\n{2}'.format(chap, atrans, "\n".join(cont))  # noqa
             return pmsg
         else:
             return errormsg
     else:
-        pmsg = 'Chapter {0} not found'.format(num)
+        pmsg = 'Chapter {0} not found'.format(chap)
         return pmsg
